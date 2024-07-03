34b549307209a975e6dd75b6dc07801847278f64
Bernardo Alves <bernardoaraujoalves@gmail.com>
2024-07-03 17:05:09 -0300

    course

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,28 @@
-const pokeApi = {}
+const pokeApi = {};
 
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 15) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
@@ -14,27 +32,3 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
 }
-
-Promise.all([                                       // transformando o pokemon em lista de novas requisições e depois utilizaremos o resultado delas
-    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-
-    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-
-    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-
-    fetch('https://pokeapi.co/api/v2/pokemon/4'),
-
-    fetch('https://pokeapi.co/api/v2/pokemon/5'),
-
-    fetch('https://pokeapi.co/api/v2/pokemon/6'),
-
-    fetch('https://pokeapi.co/api/v2/pokemon/7'),
-
-    fetch('https://pokeapi.co/api/v2/pokemon/8'),
-
-    fetch('https://pokeapi.co/api/v2/pokemon/9'),
-
-    fetch('https://pokeapi.co/api/v2/pokemon/10')
-]).then((results) => {
-    console.log(results)
-})
